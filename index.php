20c4e353b069f8a42d8df77d8bcd72e97a65a9e4
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-29 04:31:46 +0700

    tambahan style

--- index.php
+++ index.php
@@ -39,6 +39,10 @@ function tentukanGrade($nilai)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Nilai Mahasiswa</title>
     <style>
+        thead {
+            background-color: yellowgreen;
+        }
+
         footer {
             padding: 20px;
             text-align: center;
